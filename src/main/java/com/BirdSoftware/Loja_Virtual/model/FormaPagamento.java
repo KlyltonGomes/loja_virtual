f8a7fc4083d7ff6223a6ff0e5107dff564acc80c
klyltongomes <klyltongomesdesouza@MacBook-Pro-de-klylton.local>
2025-01-14 22:12:12 -0300

    criacao de Enums e text.sql para automatizar BD

---
+++ src/main/java/com/BirdSoftware/Loja_Virtual/model/FormaPagamento.java
@@ -0,0 +1,51 @@
+package com.BirdSoftware.Loja_Virtual.model;
+
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "forma_pagamento")
+@SequenceGenerator(name = "seq_forma_pagamento",sequenceName = "seq_forma_pagamento",allocationSize = 1,initialValue = 1)
+public class FormaPagamento implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_forma_pagamento")
+    private long id;
+
+    private String descricao;
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FormaPagamento that = (FormaPagamento) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
