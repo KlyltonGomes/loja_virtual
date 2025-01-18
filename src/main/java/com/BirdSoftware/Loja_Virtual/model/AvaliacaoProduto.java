2132e8415217feead2d7e7a513c5962cbc0abcb8
klyltongomes <klyltongomesdesouza@MacBook-Pro-de-klylton.local>
2025-01-18 16:14:30 -0300

    finalizado a criacao das Entitys do projeto

---
+++ src/main/java/com/BirdSoftware/Loja_Virtual/model/AvaliacaoProduto.java
@@ -0,0 +1,84 @@
+package com.BirdSoftware.Loja_Virtual.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "avaliacao_produto")
+@SequenceGenerator(name = "seq_avaliacao_produto",sequenceName = "seq_avaliacao_produto",allocationSize = 1,initialValue = 1)
+public class AvaliacaoProduto implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_avaliacao_produto")
+    private long id;
+
+    private String descricao;
+    private Integer nota;
+
+    @ManyToOne
+    @JoinColumn(name = "pessoa_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT,name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT,name = "produto_fk"))
+    private Produto produto;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AvaliacaoProduto that = (AvaliacaoProduto) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/BirdSoftware/Loja_Virtual/model/NotaFiscalVenda.java
@@ -0,0 +1,92 @@
+package com.BirdSoftware.Loja_Virtual.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "nota_fiscal_venda")
+@SequenceGenerator(name = "seq_nota_fiscal_venda",sequenceName = "seq_nota_fiscal_venda",allocationSize = 1,initialValue = 1)
+public class NotaFiscalVenda implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_venda")
+    private long id;
+
+    private String numero;
+    private String serie;
+    private String tipo;
+    @Column(columnDefinition = "TEXT")
+    private String xml;
+    @Column(columnDefinition = "TEXT")
+    private String pdf;
+
+    @OneToOne
+    @JoinColumn(name = "venda_compra_loja_virt_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT,name = "venda_compra_loja_virt_fk"))
+    private VendaCompraLojaVirtual vendaCompraLojaVirtual;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getXml() {
+        return xml;
+    }
+
+    public void setXml(String xml) {
+        this.xml = xml;
+    }
+
+    public String getPdf() {
+        return pdf;
+    }
+
+    public void setPdf(String pdf) {
+        this.pdf = pdf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NotaFiscalVenda that = (NotaFiscalVenda) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
