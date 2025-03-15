5fec8bbbc6043de3b3f932ff2497cb2731c9dcd1
klyltongomes <klyltongomesdesouza@MacBook-Pro-de-klylton.local>
2025-03-15 20:17:41 -0300

    configurando Classe Acesso

--- src/main/java/com/BirdSoftware/Loja_Virtual/controller/usuarioController.java
+++ src/main/java/com/BirdSoftware/Loja_Virtual/controller/usuarioController.java
@@ -1,8 +1,6 @@
 package com.BirdSoftware.Loja_Virtual.controller;
 
 import com.BirdSoftware.Loja_Virtual.DTO.UsuarioDTO;
-import com.BirdSoftware.Loja_Virtual.model.Usuario;
-import com.BirdSoftware.Loja_Virtual.repository.UsuarioRepository;
 import com.BirdSoftware.Loja_Virtual.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/BirdSoftware/Loja_Virtual/service/UsuarioService.java
+++ src/main/java/com/BirdSoftware/Loja_Virtual/service/UsuarioService.java
@@ -13,11 +13,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @Service
 public class UsuarioService {
